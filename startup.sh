9724f2cc4d6e2b8e06ebe1b0f0d470a6e82c1e21
Thomas Tsiakalakis <tt@dplusc.de>
2020-05-15 20:54:24 +0200

    MOAR Chrome Flags

--- startup.sh
+++ startup.sh
@@ -14,5 +14,25 @@ mkdir -p /tmp/chromium
 rm -rf /tmp/pki
 mkdir -p /tmp/pki
 
-chromium-browser --window-size=${res} --window-position=0,0 --disable-features=TranslateUI --disble-extensions --noerrdialogs --incognito --disable-sync --disable-notifications --no-experiments --disable-audio --ignore-gpu-blacklist --enable-webgl --user-data-dir=/tmp/chromium --disk-cache-dir=/tmp/chromium --disk-cache-size=0 --kiosk "${HOMEPAGE:-https://duckduckgo.com/}?name=${displayname}" >> /tmp/chromium.log 2>&1
-
+chromium-browser \
+  --window-size=${res} \
+  --window-position=0,0 \
+  --disable-features=TranslateUI \
+  --disble-extensions \
+  --disable-component-update \
+  --check-for-update-interval=31536000 \
+  --disable-restore-session-state \
+  --disable-session-crashed-bubble \
+  --noerrdialogs \
+  --incognito \
+  --disable-sync \
+  --disable-notifications \
+  --no-experiments \
+  --disable-audio \
+  --ignore-gpu-blacklist \
+  --enable-webgl \
+  --user-data-dir=/tmp/chromium \
+  --disk-cache-dir=/tmp/chromium \
+  --disk-cache-size=0 \
+  --kiosk \
+  "${HOMEPAGE:-https://duckduckgo.com/}?name=${displayname}" >> /tmp/chromium.log 2>&1
